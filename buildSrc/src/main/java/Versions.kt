c92220114c4bc88444a59e4297ef6bbf1becd696
refka-farid <refka.farid@gmail.com>
2021-11-23 22:25:47 +0100

    [Jobby] setup gradle kts

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,46 @@
+object Versions {
+    const val applicationId = "com.bravedroid.jobby"
+    //apiSDK
+    const val compileSdk = 30
+    const val targetSdk = 30
+    const val minSdk = 23
+
+    //androidx
+    const val appcompat = "1.3.1"
+    const val core_ktx = "1.6.0"
+    const val lifecycle = "2.3.0-rc01"
+
+    //kotlin
+    const val kotlin = "1.5.31"
+    const val kotlin_coroutines = "1.5.2"
+
+    //Dagger Hilt
+    const val hilt = "2.38.1"
+
+    //ui
+    const val android_material = "1.4.0"
+    const val constraintlayout = "2.1.1"
+    const val recyclerview = "1.2.1"
+
+    //data
+    const val preference = "1.1.1"
+    const val datastore_preferences = "1.0.0-alpha08"
+    const val retrofit = "2.9.0"
+    const val gson = "2.8.6"
+    const val gson_extra = "0.2.2"
+    const val okhttp = "4.9.0"
+    const val kotlinx_serialization_converter = "0.8.0"
+    const val kotlinxSerialization = "1.0.1"
+    const val room = "2.3.0"
+
+    //test
+    const val junit = "4.13.2"
+    const val core_testing = "2.1.0"
+    const val test_core_ktx = "1.4.0"
+    const val truth = "1.0.1"
+    const val mockito_core = "4.0.0"
+    const val mockito_kotlin = "4.0.0"
+
+    const val espresso_core = "3.4.0"
+    const val test_ext_junit = "1.1.3"
+}
