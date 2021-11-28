e684b88a813d07c2ed5855673f0b71c63c1de85b
refka-farid <refka.farid@gmail.com>
2021-11-28 16:52:16 +0100

    [Jobby]  add app companion module wip

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,11 +3,14 @@ val kotlinVersion = "1.5.21"
 
 plugins {
     `kotlin-dsl`
+    `java-gradle-plugin`
+     `kotlin-dsl-precompiled-script-plugins`
+
 }
 
 repositories {
-    mavenCentral()
     google()
+    mavenCentral()
 }
 
 dependencies {
