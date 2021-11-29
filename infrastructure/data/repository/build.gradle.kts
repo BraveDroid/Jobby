ceec86066cea4fc952635f91eab086c76a0d871a
refka-farid <refka.farid@gmail.com>
2021-11-29 14:33:30 +0100

    [Jobby]  auth module wip 2

--- infrastructure/data/repository/build.gradle.kts
+++ infrastructure/data/repository/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("convention-android-library-build")
 }
 
-
 dependencies {
     implementation(project(":domain"))
+    implementation(project(":infrastructure:data:datasource:auth"))
     implementation(project(":infrastructure:data:datasource:network:findwork"))
 }
