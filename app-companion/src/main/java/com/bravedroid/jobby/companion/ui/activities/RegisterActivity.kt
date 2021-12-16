ff6c7a302c55ef5031f39412d0cdc5533ec86758
refka-farid <refka.farid@gmail.com>
2021-12-16 16:12:29 +0100

    [Jobby]  add validation form

--- app-companion/src/main/java/com/bravedroid/jobby/companion/ui/activities/RegisterActivity.kt
+++ app-companion/src/main/java/com/bravedroid/jobby/companion/ui/activities/RegisterActivity.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.lifecycleScope
 import com.bravedroid.jobby.companion.databinding.ActivityRegisterBinding
 import com.bravedroid.jobby.companion.vm.RegisterViewModel
+import com.bravedroid.jobby.domain.log.Logger
+import com.bravedroid.jobby.domain.log.Priority
 import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RegisterActivity : AppCompatActivity() {
+    @Inject
+    internal lateinit var logger: Logger
     private val viewModel: RegisterViewModel by viewModels()
     private lateinit var binding: ActivityRegisterBinding
+
+    private val nameSharedFlow: MutableSharedFlow<String> = MutableSharedFlow()
+    private val emailSharedFlow: MutableSharedFlow<String> = MutableSharedFlow()
+    private val passwordSharedFlow: MutableSharedFlow<String> = MutableSharedFlow()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +61,28 @@ class RegisterActivity : AppCompatActivity() {
             binding.registerBtn.isEnabled = true
         }.launchIn(lifecycleScope)
 
+
+        binding.editTextUserName.doOnTextChanged { text, _, _, _ ->
+            lifecycleScope.launch {
+                if (text != null) nameSharedFlow.emit(text.toString())
+            }
+        }
+        binding.editTextEmail.doOnTextChanged { text, _, _, _ ->
+            lifecycleScope.launch {
+                if (text != null) emailSharedFlow.emit(text.toString())
+            }
+        }
+        binding.editTextPassword.doOnTextChanged { text, _, _, _ ->
+            lifecycleScope.launch {
+                if (text != null) passwordSharedFlow.emit(text.toString())
+            }
+        }
+
+        viewModel.validateRegisterForm(nameSharedFlow, emailSharedFlow, passwordSharedFlow)
+            .onEach { isValid ->
+                logger.log("RegisterActivity", "$isValid", Priority.V)
+                binding.registerBtn.isEnabled = isValid
+            }.launchIn(lifecycleScope)
     }
 
     private fun navigateToLogin() {
--- app-companion/src/main/java/com/bravedroid/jobby/companion/vm/RegisterViewModel.kt
+++ app-companion/src/main/java/com/bravedroid/jobby/companion/vm/RegisterViewModel.kt
@@ -2,14 +2,16 @@ package com.bravedroid.jobby.companion.vm
 
 import androidx.lifecycle.ViewModel
 import com.bravedroid.jobby.companion.CoroutineProvider
+import com.bravedroid.jobby.companion.vm.FlowExt.combineOn
 import com.bravedroid.jobby.domain.log.Logger
+import com.bravedroid.jobby.domain.log.Priority
 import com.bravedroid.jobby.domain.usecases.RegisterUserUseCase
 import com.bravedroid.jobby.domain.usecases.RegisterUserUseCase.RegistrationResponse
 import com.bravedroid.jobby.domain.utils.DomainResult
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,7 +20,7 @@ class RegisterViewModel @Inject constructor(
     private val coroutineProvider: CoroutineProvider,
     private val registerUserUseCase: RegisterUserUseCase,
     private val logger: Logger,
-    ) : ViewModel() {
+) : ViewModel() {
 
     private val _uiEventFlow: MutableSharedFlow<UiEvent> = MutableSharedFlow()
     val uiEventFlow: SharedFlow<UiEvent> = _uiEventFlow
@@ -28,7 +30,7 @@ class RegisterViewModel @Inject constructor(
             registerUserUseCase(model.toRegistrationRequest()).collectLatest {
                 when (it) {
                     is DomainResult.Error -> {
-                        logger.log("RegisterViewModel","${it.errorEntity}")
+                        logger.log("RegisterViewModel", "${it.errorEntity}")
                         _uiEventFlow.emit(
                             UiEvent.ShowError(
                                 "Unknown Error !"
@@ -36,8 +38,8 @@ class RegisterViewModel @Inject constructor(
                         )
                     }
                     is DomainResult.Success -> {
-                    if (it.data== RegistrationResponse.Registered)
-                        _uiEventFlow.emit(UiEvent.NavigationToLoginScreen)
+                        if (it.data == RegistrationResponse.Registered)
+                            _uiEventFlow.emit(UiEvent.NavigationToLoginScreen)
                     }
                 }
             }
@@ -56,8 +58,48 @@ class RegisterViewModel @Inject constructor(
         password,
     )
 
+    fun validateRegisterForm(
+        nameSharedFlow: Flow<String>,
+        emailSharedFlow: Flow<String>,
+        passwordSharedFlow: Flow<String>
+    ): Flow<Boolean> = combineOn(
+        Dispatchers.Default,
+        nameSharedFlow,
+        emailSharedFlow,
+        passwordSharedFlow
+    ) { name, email, password ->
+        logger.log(
+            tag = "RegisterViewModel",
+            msg = "name: $name, email: $email, password: $password",
+            priority = Priority.V
+        )
+        validateForm(name, email, password)
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        val isValidName = name.isNotBlank()
+        val isValidEmail = email.isNotBlank() && email.contains("@")
+        val isValidPassword = password.isNotBlank() && password.length >= 8
+        return isValidName && isValidEmail && isValidPassword
+    }
+
     sealed class UiEvent {
         data class ShowError(val errorMessage: String) : UiEvent()
         object NavigationToLoginScreen : UiEvent()
     }
+}
+
+object FlowExt {
+    fun <T1, T2, T3, R> combineOn(
+        dispatcher: CoroutineDispatcher,
+        flow: Flow<T1>,
+        flow2: Flow<T2>,
+        flow3: Flow<T3>,
+        transform: suspend (T1, T2, T3) -> R,
+    ): Flow<R> = combine(
+        flow.flowOn(dispatcher),
+        flow2.flowOn(dispatcher),
+        flow3.flowOn(dispatcher),
+        transform
+    )
 }
