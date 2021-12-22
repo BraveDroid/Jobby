ee89df8e4953c7f6ee3da79ccbeb133fc0f78768
refka-farid <refka.farid@gmail.com>
2021-12-22 20:22:40 +0100

    [Jobby]  Feature login wip 3

---
+++ app-companion/src/main/java/com/bravedroid/jobby/companion/ui/activities/AllFontTextAppearanceActivity.kt
@@ -0,0 +1,12 @@
+package com.bravedroid.jobby.companion.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bravedroid.jobby.companion.R
+
+class AllFontTextAppearanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_font_text_appearance)
+    }
+}
